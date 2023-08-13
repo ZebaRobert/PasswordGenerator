2fc28757a455eea1beb83f40e19a601ccdcdb8dd
ZebaRobert <zeba.robert1@gmail.com>
2023-08-13 16:48:05 +0200

    Create class method load_from()

--- run.py
+++ run.py
@@ -25,8 +25,38 @@ class User :
             with open(file_path, "wb") as file:
                 pickle.dump(self, file)
         except FileExistsError:
-             print(f"User already exists under ({self.username}) username. Please try to login")
+             print(f"User already exists under ({self.username}) username. Please try to login\n")
         except Exception as e:
-            print(f"An errorr occured: {e}")
+            print(f"An errorr occured: {e}\n")
+    
+    @classmethod
+    def load_from(cls, folder_path, username, password):
+        """
+        Loads an instance of the class user from the users folder
+        """
+        try:
+            if not os.path.exists(folder_path):
+                os.makedirs(folder_path)
+            file_path = os.path.join(folder_path, username) + ".pkl"
+            with open(file_path, "rb") as file:
+                saved_data = pickle.load(file)
+                user_password = saved_data.get("password")
+                if password == user_password:
+                    return saved_data
+                else:
+                    retry_count = 3
+                    while retry_count > 0:
+                        retry = input(f"Incorrect password. {retry_count} attempts remaining. Please try again:\n")
+                        saved_data = User.load_from(FOLDER_PATH, username, retry)
+                        if saved_data is not None:
+                            return saved_data
+                        retry_count -= 1
+                    print("Maximum password attempts made. Please create a new account\n")
+                    
+        except FileNotFoundError:
+             print(f"User under the ({username}) username was not found. Please create an account.\n")
+        except Exception as e:
+            print(f"An errorr occured: {e}\n")
+
             
         

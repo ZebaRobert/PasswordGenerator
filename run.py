91ac100404c0bbe935a0eef7aeb301ce7c326989
ZebaRobert <zeba.robert1@gmail.com>
2023-08-13 15:58:58 +0200

    Create class method save_to()

--- run.py
+++ run.py
@@ -2,6 +2,8 @@ import pickle
 import hashlib
 import os
 
+FOLDER_PATH = 'users'
+
 class User : 
 
     def __init__(self, username, password):
@@ -11,3 +13,20 @@ class User :
         self.username = username
         self.password = password
         self.filename = f"{username}.pkl"
+
+    def save_to(self, filename, folder_path):
+        """
+        Saves an instance of the class inside users folder
+        """
+        try:
+            if not os.path.exists(folder_path):
+                os.makedirs(folder_path)
+            file_path = os.path.join(folder_path, filename)
+            with open(file_path, "wb") as file:
+                pickle.dump(self, file)
+        except FileExistsError:
+             print(f"User already exists under ({self.username}) username. Please try to login")
+        except Exception as e:
+            print(f"An errorr occured: {e}")
+            
+        
